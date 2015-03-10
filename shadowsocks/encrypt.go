90a8eaf9e26355a13b83c02055bd28544cb1d9b8
genzj <zj0512@gmail.com>
2015-03-10 07:40:38 +0000

    pick chacha20 support from github defia/shadowsocks-go

--- shadowsocks/encrypt.go
+++ shadowsocks/encrypt.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"golang.org/x/crypto/blowfish"
 	"golang.org/x/crypto/cast5"
+	"github.com/codahale/chacha20"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/des"
@@ -137,6 +138,10 @@ func newRC4MD5Stream(key, iv []byte, _ DecOrEnc) (cipher.Stream, error) {
 	return rc4.NewCipher(rc4key)
 }
 
+func newChaCha20Stream(key, iv []byte, _ DecOrEnc) (cipher.Stream, error) {
+	return chacha20.New(key, iv)
+}
+
 type cipherInfo struct {
 	keyLen    int
 	ivLen     int
@@ -153,6 +158,7 @@ var cipherMethod = map[string]*cipherInfo{
 	"rc4-md5":     {16, 16, newRC4MD5Stream},
 	"rc4":         {16, 0, nil},
 	"table":       {16, 0, nil},
+	"chacha20":    {32, 8, newChaCha20Stream},
 }
 
 func CheckCipherMethod(method string) error {
